6958bafaefe4c9ae38227f8b4318a3e7f80d634d
Harsh Srivastava <91286627+harshsri-14@users.noreply.github.com>
2022-03-26 20:08:40 +0530

    Create Two Sum.py

---
+++ Day_1/Two Sum.py
@@ -0,0 +1,8 @@
+# problem link: https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)-1) :
+            if target - nums[i] in nums[i+1:] :
+                idx = nums[i+1:].index(target - nums[i])
+                return (i, idx+i+1)
